cb1635b00bc8814f3c72285ad3b923a709a3c651
scorpionhiccup <sharvil.2009@gmail.com>
2015-08-31 20:56:47 +0000

    Updating the README

--- src/main.sh
+++
@@ -1,8 +0,0 @@
-source /usr/local/bin/virtualenvwrapper.sh
-workon cloud_project
-pip install -r ../requirements.txt
-python db_create.py
-python db_migrate.py
-#python app/flask_app.py app/ips.txt app/image_location_files.txt app/types.json
-#sudo mkdir -p /var/lib/libvirt/save
-#mkdir -p ./srv/ceph/{osd,mon,mds}
